70f68eba963b76405c0c00572547545ea3deb505
divinerapier <poriter.coco@gmail.com>
2019-07-28 10:26:30 +0800

    feature(gateway): upload to dynamic path
    Signed-off-by: divinerapier <poriter.coco@gmail.com>

--- onyxia-gateway/src/main.rs
+++ onyxia-gateway/src/main.rs
@@ -1,9 +1,11 @@
+use actix_http::httpmessage::HttpMessage;
+use actix_web::FromRequest;
 use std::cell::Cell;
 use std::fs;
 use std::io::Write;
 
 use actix_multipart::{Field, Multipart, MultipartError};
-use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use futures::future::{err, Either};
 use futures::{Future, Stream};
 
@@ -11,9 +13,16 @@ pub struct AppState {
     pub counter: Cell<usize>,
 }
 
-pub fn save_file(field: Field) -> impl Future<Item = i64, Error = Error> {
+pub fn save_file(field: Field, filepath: &str) -> impl Future<Item = i64, Error = Error> {
+    println!("disposition: {:?}", field.content_disposition());
+    println!("content_type: {:?}", field.content_type());
+    println!("headers: {:?}", field.headers());
+
     let current_dir = std::env::current_dir().unwrap();
-    let file_path_string = current_dir.join("upload.data");
+    let file_path_string = current_dir.join(filepath);
+    println!("upload file to path: {:?}", file_path_string);
+    let parent = file_path_string.parent().unwrap();
+    std::fs::create_dir_all(parent);
     let file = match fs::File::create(file_path_string) {
         Ok(file) => file,
         Err(e) => return Either::A(err(error::ErrorInternalServerError(e))),
@@ -45,15 +54,18 @@ pub fn save_file(field: Field) -> impl Future<Item = i64, Error = Error> {
 }
 
 pub fn upload(
+    req: HttpRequest,
     multipart: Multipart,
     counter: web::Data<Cell<usize>>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     counter.set(counter.get() + 1);
     println!("{:?}", counter.get());
+    let path = req.match_info().get("path").unwrap().to_owned();
+    println!("path: {}", path);
 
     multipart
         .map_err(error::ErrorInternalServerError)
-        .map(|field| save_file(field).into_stream())
+        .map(move |field| save_file(field, &path).into_stream())
         .flatten()
         .collect()
         .map(|sizes| HttpResponse::Ok().json(sizes))
@@ -65,11 +77,20 @@ pub fn upload(
 
 fn index() -> HttpResponse {
     let html = r#"<html>
-        <head><title>Upload Test</title></head>
+        <head><title>Upload Test</title>
+            <script type="text/javascript">
+                function upload(input) {
+                    var filename = document.getElementById('FileName').value;
+                    console.log('filename = ' + filename);
+                    document.getElementById('UploadForm').action = filename
+                }
+            </script>
+        </head>
         <body>
-            <form target="/" method="post" enctype="multipart/form-data">
-                <input type="file" name="file"/>
-                <input type="submit" value="Submit"></button>
+            <form target="_blank" method="post" enctype="multipart/form-data" id="UploadForm" onsubmit="return upload(this);">
+                <input type="input" name="name" id="FileName"></input>
+                <input type="file" name="file" id="FileContent"></input>
+                <input type="submit" value="Submit" ></button>
             </form>
         </body>
     </html>"#;
@@ -86,10 +107,12 @@ fn main() -> std::io::Result<()> {
             .data(Cell::new(0usize))
             .wrap(middleware::Logger::default())
             .service(
-                web::resource("/")
+                // FIXME: can not represent the path who has a slash
+                web::resource("/{path}")
                     .route(web::get().to(index))
                     .route(web::post().to_async(upload)),
             )
+            .service(web::resource("/").route(web::get().to(index)))
     })
     .bind("127.0.0.1:8080")?
     .run()
