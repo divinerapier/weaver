666c42b33464331656f959bb59c727ae26344a64
divinerapier <poriter.coco@gmail.com>
2019-08-03 19:28:18 +0800

    feature(lib): load index data
    Signed-off-by: divinerapier <poriter.coco@gmail.com>

--- libonyxia/src/index.rs
+++ libonyxia/src/index.rs
@@ -1,7 +1,28 @@
 #[allow(dead_code)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq)]
 pub struct Index {
     pub path: String,
-    pub volume: usize,
     pub offset: usize,
     pub length: usize,
+}
+
+impl Index {
+    pub fn new(path: String, offset: usize, length: usize) -> Index {
+        Index {
+            path,
+            offset,
+            length,
+        }
+    }
+}
+
+pub struct RawIndex {
+    pub offset: usize,
+    pub length: usize,
+}
+
+impl RawIndex {
+    pub fn new(offset: usize, length: usize) -> RawIndex {
+        RawIndex { offset, length }
+    }
 }
