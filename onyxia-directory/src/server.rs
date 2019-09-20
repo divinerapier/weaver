c7c6b0bcb980bf8473adee95b72a1d664f76a21d
divinerapier <poriter.coco@gmail.com>
2019-09-20 19:12:47 +0800

    feature: add response status
    Signed-off-by: divinerapier <poriter.coco@gmail.com>

--- onyxia-directory/src/server.rs
+++ onyxia-directory/src/server.rs
@@ -1,17 +1,17 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 
 use onyxia_proto::directory::directory;
 use onyxia_proto::directory::directory_grpc;
 
 #[derive(Clone)] // clone trait required by `fn create_directory`
 pub struct DirectoryService {
-    dir: Arc<Mutex<libonyxia::directory::Directory>>,
+    dir: Arc<RwLock<libonyxia::directory::Directory>>,
 }
 
 impl DirectoryService {
     pub fn new() -> DirectoryService {
         DirectoryService {
-            dir: Arc::new(Mutex::new(libonyxia::directory::Directory::new())),
+            dir: Arc::new(RwLock::new(libonyxia::directory::Directory::new())),
         }
     }
 }
@@ -33,10 +33,35 @@ impl directory_grpc::Directory for DirectoryService {
     ) {
         let replication_count = req.get_replication_count();
         let dir = self.dir.clone();
-        let dir: std::sync::MutexGuard<'_, libonyxia::directory::Directory> = dir.lock().unwrap();
+        let dir: std::sync::RwLockReadGuard<'_, libonyxia::directory::Directory> =
+            dir.read().unwrap();
+        let mut resp = directory::AssignResponse::new();
+
+        if replication_count <= 0 {
+            resp.mut_status()
+                .set_message(format!("invalid replication count: {}", replication_count));
+            resp.mut_status().set_status_code(400);
+            sink.send();
+            return;
+        }
+
+        if dir.volumes.len() < replication_count as usize {
+            return;
+        }
+
         for i in 0..replication_count as usize {
             match dir.random_writable_volume() {
-                Some(i) => {}
+                Some(volume_id) => {
+                    if i == 0 {
+                        let mut master = directory::Replica::new();
+                        master.set_volume_id(volume_id);
+                        resp.set_master(master);
+                    } else {
+                        let mut replica = directory::Replica::new();
+                        replica.set_volume_id(volume_id);
+                        resp.mut_slaves().push(replica);
+                    }
+                }
                 None => {}
             }
         }
